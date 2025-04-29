c551e755efd62e62f6f3d06420c302931c032b38
EmilySillars <emsillars@gmail.com>
2025-04-29 17:15:45 +0200

    fix gitignore

---
+++ peek_at_snitch_assembly.py
@@ -0,0 +1,235 @@
+from xdsl.backend.riscv.lowering import (
+    convert_arith_to_riscv,
+    convert_func_to_riscv_func,
+    convert_memref_to_riscv,
+    convert_scf_to_riscv_scf,
+)
+from xdsl.backend.riscv.lowering.convert_riscv_scf_to_riscv_cf import (
+    ConvertRiscvScfToRiscvCfPass,
+)
+from xdsl.backend.riscv.lowering.convert_snitch_stream_to_snitch import (
+    ConvertSnitchStreamToSnitch,
+)
+from xdsl.builder import ImplicitBuilder
+from xdsl.context import Context
+from xdsl.dialects import arith, func, linalg
+from xdsl.dialects.builtin import AffineMap, AffineMapAttr, MemRefType, ModuleOp, f64
+from xdsl.dialects.riscv import riscv_code
+from xdsl.interpreters.utils.ptr import TypedPtr
+from xdsl.ir import Attribute, Block, Region, SSAValue
+from xdsl.passes import PipelinePass
+from xdsl.tools.command_line_tool import get_all_dialects
+from xdsl.transforms import (
+    arith_add_fastmath,
+    convert_linalg_to_loops,
+    convert_linalg_to_memref_stream,
+    convert_memref_stream_to_loops,
+    convert_memref_stream_to_snitch_stream,
+    convert_riscv_scf_for_to_frep,
+    dead_code_elimination,
+    loop_hoist_memref,
+    lower_affine,
+    memref_streamify,
+    reconcile_unrealized_casts,
+)
+from xdsl.transforms.canonicalize import CanonicalizePass
+from xdsl.transforms.lower_snitch import LowerSnitchPass
+from xdsl.transforms.mlir_opt import MLIROptPass
+from xdsl.transforms.riscv_register_allocation import RISCVRegisterAllocation
+from xdsl.transforms.riscv_scf_loop_range_folding import (
+    RiscvScfLoopRangeFoldingPass,
+)
+from xdsl.transforms.snitch_register_allocation import SnitchRegisterAllocation
+from collections import Counter
+from dataclasses import dataclass, field
+from xdsl.transforms.test_lower_linalg_to_snitch import LOWER_SNITCH_STREAM_TO_ASM_PASSES
+from xdsl.transforms.test_lower_linalg_to_snitch import LOWER_MEMREF_STREAM_TO_SNITCH_STREAM_PASSES, OPTIMISE_MEMREF_STREAM_PASSES
+from io import StringIO
+from xdsl.dialects.riscv import RISCVAsmOperation, RsRsOffIntegerOperation, MulOp, LiOp
+from xdsl.dialects.riscv_cf import ConditionalBranchOperation
+from xdsl.dialects.riscv_func import FuncOp
+from xdsl.dialects.riscv_snitch import FrepOuterOp
+from xdsl.passes import ModulePass
+
+def createMatmulTransposeB(
+    m=0, outputVectorEltCount=40, inputVectorEltCount=100
+):
+    if m == 0:
+        m = 1
+    k = inputVectorEltCount
+    n = outputVectorEltCount // 8
+    if (n) * 8 != outputVectorEltCount:
+        raise Exception("Sorry, only row dimensions divisble by 8 allowed!")
+    ctx = Context()
+    for dialect_name, dialect_factory in get_all_dialects().items():
+        ctx.register_dialect(dialect_name, dialect_factory)
+    a_shape2 = (m, k)
+    b_shape2 = (n, k)
+    c_shape2 = (m, n)
+    # Let's try to recreate the parsed matmultranspose_b using an xDSL IR builder...
+    a_type2 = MemRefType(f64, a_shape2)
+    b_type2 = MemRefType(f64, b_shape2)
+    c_type2 = MemRefType(f64, c_shape2)
+    kernel_op2 = func.FuncOp(
+        "matmul_transpose_b", ((a_type2, b_type2, c_type2), ())
+    )
+    with ImplicitBuilder(kernel_op2.body) as (a2, b2, c2):
+        # Add name hints to make it easier to track how values are lowered
+        a2.name_hint = "A"
+        b2.name_hint = "B"
+        c2.name_hint = "C"
+        body2 = Region(Block(arg_types=(f64, f64, f64)))
+        linalg.GenericOp(
+            inputs=(a2, b2),
+            outputs=(c2,),
+            body=body2,
+            indexing_maps=(
+                AffineMapAttr(AffineMap.from_callable(lambda m, n, k: (m, k))),
+                AffineMapAttr(AffineMap.from_callable(lambda m, n, k: (n, k))),
+                AffineMapAttr(AffineMap.from_callable(lambda m, n, k: (m, n))),
+            ),
+            iterator_types=(
+                linalg.IteratorTypeAttr.parallel(),
+                linalg.IteratorTypeAttr.parallel(),
+                linalg.IteratorTypeAttr.reduction(),
+            ),
+        )
+        with ImplicitBuilder(body2) as (a_val2, b_val2, acc_old_val2):
+            prod_val2 = arith.MulfOp(a_val2, b_val2).result
+            acc_new_val2 = arith.AddfOp(acc_old_val2, prod_val2).result
+            linalg.YieldOp(acc_new_val2)
+            # Add more name hints to make it easier to track how values are lowered
+            a_val2.name_hint = "a"
+            b_val2.name_hint = "b"
+            acc_old_val2.name_hint = "acc_old"
+            prod_val2.name_hint = "prod"
+            acc_new_val2.name_hint = "acc_new"
+        func.ReturnOp()
+
+    linalg_module = ModuleOp((kernel_op2,))
+    my_linalg_to_snitch = PipelinePass(
+        [
+            convert_linalg_to_memref_stream.ConvertLinalgToMemRefStreamPass(),
+            arith_add_fastmath.AddArithFastMathFlagsPass(),
+            *OPTIMISE_MEMREF_STREAM_PASSES,
+            *LOWER_MEMREF_STREAM_TO_SNITCH_STREAM_PASSES,
+            convert_riscv_scf_for_to_frep.ConvertRiscvScfForToFrepPass(),
+            *LOWER_SNITCH_STREAM_TO_ASM_PASSES,
+        ]
+    )
+
+    my_asm_module = run_thru_all_passes(
+        my_linalg_to_snitch.passes,
+        linalg_module,  ctx,
+    )
+
+    return (linalg_module, my_asm_module, m, n, k)
+
+
+@dataclass
+class HardwareLoop:
+    """Class for keeping track of hardware loop characteristics"""
+    name: str = FrepOuterOp.name
+    loop_repeats: int = 1 # number of times loop executes
+    body_size: int = 1    # number of instructions in body of the loop
+
+@dataclass
+class EnclosingSCFLoop:
+    """Class for keeping track of a potential loop surrounding the hardware"""
+    name: str = "an enclosing loop"
+    iters : int = 1   # number of times the enclosing loop executes
+    exists : bool = False # whether the hardware loop is in fact enclosed by another loop
+
+def look_for_frep(module: ModuleOp, output = StringIO()) -> (list, str):
+    freps = []
+    frepsAsHLs = []
+    for op in module.body.walk():
+        assert (isinstance(op, RISCVAsmOperation)or(isinstance(op, FuncOp))), f"{op}"
+        if op.name == FrepOuterOp.name:
+            freps.append(op)
+    for op in freps:
+        asm = op.assembly_line()
+        if asm is not None:
+            print(asm, file=output)
+            if 'immediate' in op.max_rep.op.attributes:
+                imm = op.max_rep.op.attributes['immediate'].value.data
+                print(f'this frep was passed an immediate value of {imm}',file=output)
+                frepsAsHLs.append(HardwareLoop(loop_repeats=imm + 1, body_size= op.max_inst))
+            else:
+                print(f'ERROR: op.max_rep.op: {op.max_rep.op}',file=output)
+            print(f'max_inst is {op.max_inst}',file=output)
+    if len(freps) == 0:
+        print(f'ERROR no frep found in lowering!!',file=output)
+    return (frepsAsHLs, output.getvalue())
+
+def look_for_enclosing_loop(module: ModuleOp, output = StringIO()) -> (EnclosingSCFLoop,str):
+    mulsBefore = []
+    mulsAfter = []
+    sawFrep = False
+    frepCount = 0
+    branches = []
+    for op in module.body.walk():
+        assert (isinstance(op, RISCVAsmOperation)or(isinstance(op, FuncOp))), f"{op}"
+        if op.name == FrepOuterOp.name:
+            frepCount = frepCount + 1
+        if isinstance(op, ConditionalBranchOperation): 
+            branches.append(op)
+    if len(branches) > 1:
+        print(f'ERROR: more than one branch found!!',file=output)
+    if frepCount == 0:   
+        print(f'ERROR no frep found in lowering!!',file=output)
+    if frepCount > 1:   
+        print(f'ERROR more than one frep found in lowering!!',file=output)
+    if len(branches) == 0:
+        return (EnclosingSCFLoop(exists=False),output.getvalue())
+    limb = branches[0] 
+    print(f'rs1: {limb.rs1}, rs2: {limb.rs2}',file=output)
+    print(f'rs1.op: {limb.rs1.op}, rs2.op: {limb.rs2.op}',file=output)
+    if limb.rs2.op.name != LiOp.name:
+        return (EnclosingSCFLoop(exists=False),output.getvalue())
+    imm = limb.rs2.op.attributes['immediate'].value.data 
+    return (EnclosingSCFLoop(limb.name, iters=imm,exists=True), output.getvalue())
+
+def check_lowered_matvec_tiling(firstDim : int, outputVectorEltCount : int, inputVectorEltCount : int):
+    if firstDim != 0 and firstDim != 1:
+        raise Exception("MatVEC requires first dimension M = 1")
+    if firstDim == 0:
+        firstDim = 1;
+    expectedFMADDs = outputVectorEltCount*inputVectorEltCount
+    res = False
+    # create linalg, then lower to assembly
+    linalg_mod, asm_mod, m, n, k = createMatmulTransposeB(firstDim, outputVectorEltCount, inputVectorEltCount)
+    # look for a hardware loop
+    loops, commentary = look_for_frep(asm_mod)
+    if len(loops) != 1:
+        raise Exception(f'ERROR: expected only ONE hardware loop, but found {len(loops)} {commentary}')
+    # look for an enclosing loop
+    oLoop, commentary2 = look_for_enclosing_loop(asm_mod)
+    if not oLoop.exists:
+        res = expectedFMADDs == (n*k*8)
+    res = expectedFMADDs == ((loops[0].body_size * loops[0].loop_repeats) * oLoop.iters*8)
+    return (res,loops[0],oLoop)
+
+def run_thru_all_passes(
+    passes: tuple[ModulePass, ...], module: ModuleOp, ctx: Context
+) -> ModuleOp:
+    res = module.clone()
+    for p in passes:
+        p.apply(ctx, res)
+    return res
+
+def main():
+    print("\n|----------- HOODLE ----------|",end='\n')
+    print(check_lowered_matvec_tiling(0,40,100))
+    print(check_lowered_matvec_tiling(0,64,80))
+   
+
+
+
+if __name__ == "__main__":
+    main()
+
+# Dependencies: Python3, xdsl
+# python3 -m venv myenv
+# source myenv/bin/activate
+# pip install xdsl[riscv]
---
+++ tile_size_gen.py
@@ -0,0 +1,293 @@
+from itertools import product
+import pandas as pd
+
+inputVectorEltCount = 400 # M
+outputVectorEltCount = 1200 # N
+dispatchName = "dispatch_1"
+
+# inputVectorEltCount = 600
+# outputVectorEltCount = 600
+# dispatchName = "dispatch_8"
+
+# inputVectorEltCount = 400
+# outputVectorEltCount = 600
+# dispatchName = "dispatch_7"
+
+# inputVectorEltCount = 600
+# outputVectorEltCount = 161
+# dispatchName = "dispatch_9"
+
+def roundUpToNearestMultipleOf(num, row_dim):
+  remainder = num % row_dim
+  if (remainder != 0):
+    return (1200//row_dim) * row_dim + row_dim
+  else:
+    return num
+
+def weightMatTileSize(row_dim, reduction_dim):
+  return row_dim * reduction_dim
+
+def spaceForTiles(row_dim, reduction_dim):
+  # space in element count
+  inputVectorTile = 1*reduction_dim
+  weightMatTiles = 2*weightMatTileSize(row_dim,reduction_dim)
+  space = inputVectorTile + weightMatTiles
+  # space in  bytes
+  spaceInBytes = space * 8
+  #print(f'0-{48}-{100}:\ninputVectorTile elts: {inputVectorTile} * 8 = {inputVectorTile*8}. \nweightMatTiles elts: {weightMatTiles} * 8 = {weightMatTiles*8}.\ntotal in bytes: {spaceInBytes}.')
+  return spaceInBytes
+
+def spaceRemaining(row_dim, reduction_dim):
+  l1MemoryBytes = 100000
+  outputMatVec = roundUpToNearestMultipleOf(outputVectorEltCount, row_dim) * 8
+  outputFusedAdd = outputVectorEltCount *8
+  inputFusedAdd = outputVectorEltCount *8
+  remaining = l1MemoryBytes - outputMatVec - outputFusedAdd - inputFusedAdd - spaceForTiles(row_dim, reduction_dim)
+  return remaining
+
+def smallEnough(row_dim, red_dim):
+  return (spaceRemaining(row_dim, red_dim)> 0)
+
+def dividesIntoOutputVectorEltCount(num):
+  return (outputVectorEltCount % num == 0)
+
+def dividesIntoInputVectorEltCount(num):
+  return (inputVectorEltCount % num == 0)
+
+def rowDimOptions(c='n'):
+  hardware_loop_body_options = [3,4,5,6,7,8,9,10,11,12,13,14]
+  byEight = list(map(lambda x: 8*x,hardware_loop_body_options))
+  max = outputVectorEltCount
+  min = byEight[0]
+  exhaustive = list(range(min, max, 8))
+  if c == 'p':
+    print('rowDimOptions')
+    print(f'\tMinimum: 3*8 = {min} \n\tMaximum: N = {max}\n',end='')
+    print(f'\t{len(byEight)} sensible options: ')
+    print(f'\t{byEight}')
+    print(f'\t{len(exhaustive)} total options: ')
+    print(f'\t{exhaustive}')
+    print("\tReturning Exhaustive.")
+  return exhaustive
+
+def reductionDimOptions(c='n'):
+  max = inputVectorEltCount
+  min = 8
+  sensible = list(range(min, (inputVectorEltCount//2)+1))
+  exhaustive = list(range(min, inputVectorEltCount+1))
+  if (inputVectorEltCount % 2) != 0:
+    print(f"WARNING: M = {inputVectorEltCount} is NOT divisible by 2!")
+  if c == 'p':
+    print('reductionDimOptions')
+    print(f'\tMinimum: {min} \n\tMaximum: M = {max}\n',end='')
+    print(f'\t{len(sensible)} sensible options: ')
+    print(f'\t{sensible}')
+    print(f'\t{len(exhaustive)} total options: ')
+    print(f'\t{exhaustive}')
+    print("\tReturning Exhaustive.")
+  return exhaustive
+
+def case_1_exhaustive_options(c='n'):
+  row_dim_no_padding = list(filter(lambda x: dividesIntoOutputVectorEltCount(x), rowDimOptions()))
+  reduction_dim_no_padding = list(filter(lambda x: dividesIntoInputVectorEltCount(x), reductionDimOptions()))
+  exhaustive = list(product(row_dim_no_padding, reduction_dim_no_padding))
+  if c=='p':
+    print(f'case_1_exhaustive_options')
+    print(f'\tlen {len(row_dim_no_padding)}; row_dim_no_padding: {row_dim_no_padding}')
+    print(f'\tlen {len(reduction_dim_no_padding)}; reduction_dim_no_padding: {reduction_dim_no_padding}')
+  return exhaustive
+  
+
+def case_1_pruned_options(c='n'):
+  row_dim_no_padding = list(filter(lambda x: dividesIntoOutputVectorEltCount(x), rowDimOptions()))
+  reduction_dim_no_padding = list(filter(lambda x: dividesIntoInputVectorEltCount(x), reductionDimOptions()))
+  if (inputVectorEltCount % 2) != 0:
+    print(f"WARNING: M = {inputVectorEltCount} is NOT divisible by 2!")
+  reduction_dim_halve_options_for_double_buffering = list(filter(lambda x: x <= (inputVectorEltCount//2)+1, reduction_dim_no_padding))
+  greaterThan20 = list(filter(lambda x: x > 20, reduction_dim_halve_options_for_double_buffering))
+  pruned = list(product(row_dim_no_padding, greaterThan20))
+  if c == 'p':
+    print('case_1_pruned_options')
+    print(f'\tlen {len(row_dim_no_padding)}; row dim unchanged: {row_dim_no_padding}')
+    print(f'\tlen {len(reduction_dim_halve_options_for_double_buffering)}; reduction dims pruned for double buffering: {reduction_dim_halve_options_for_double_buffering}')
+    print(f'\tlen {len(greaterThan20)}; pruned away reduction dims < 20: {greaterThan20}')
+  return pruned
+
+def case_2_exhaustive_options(c='n'):
+  row_dim_no_padding = list(filter(lambda x: dividesIntoOutputVectorEltCount(x), rowDimOptions()))
+  # STRICTLY CASE 2
+  reduction_dim_no_padding = list(filter(lambda x: dividesIntoInputVectorEltCount(x), reductionDimOptions()))
+  reduction_dim_w_o_case_1 = list(set(reductionDimOptions()).difference(set(reduction_dim_no_padding)))
+  exhaustive = list(product(row_dim_no_padding, reduction_dim_w_o_case_1))
+  if c=='p':
+    print(f'case_2_exhaustive_options')
+    print(f'\tlen {len(row_dim_no_padding)}; row_dim_no_padding: {row_dim_no_padding}')
+    print(f'\tlen {len(reduction_dim_w_o_case_1)}; reduction_dim_w_o_case_1: {reduction_dim_w_o_case_1}')
+  return exhaustive
+
+def case_2_pruned_options(c='n'):
+  row_dim_no_padding = list(filter(lambda x: dividesIntoOutputVectorEltCount(x), rowDimOptions()))
+  # STRICTLY CASE 2
+  reduction_dim_no_padding = list(filter(lambda x: dividesIntoInputVectorEltCount(x), reductionDimOptions()))
+  reduction_dim_w_o_case_1 = list(set(reductionDimOptions()).difference(set(reduction_dim_no_padding)))
+  if (inputVectorEltCount % 2) != 0:
+    print(f"WARNING: M = {inputVectorEltCount} is NOT divisible by 2!")
+  reduction_dim_halve_options_for_double_buffering = list(filter(lambda x: x <= (inputVectorEltCount//2)+1, reduction_dim_w_o_case_1))
+  reduction_dim_halve_options_for_double_buffering.sort() # sort from least to greatest before taking every 10th
+  reduction_dim_every_tenth= []
+  for i in range(0,len(reduction_dim_halve_options_for_double_buffering)):
+    if (i % 10) == 0:
+      reduction_dim_every_tenth.append(reduction_dim_halve_options_for_double_buffering[i])
+  greaterThan20 = list(filter(lambda x: x > 20, reduction_dim_every_tenth))
+  exhaustive = list(product(row_dim_no_padding, greaterThan20))
+  if c=='p':
+    print(f'case_2_pruned_options')
+    print(f'\tlen {len(row_dim_no_padding)}; row dim left unchanged: {row_dim_no_padding}')
+    print(f'\tlen {len(reduction_dim_w_o_case_1)}; reduction dim with case 1 pruned away: {reduction_dim_w_o_case_1}')
+    print(f'\tlen {len(reduction_dim_halve_options_for_double_buffering)}; reduction dims pruned for double buffering: {reduction_dim_halve_options_for_double_buffering}')
+    print(f'\tlen {len(reduction_dim_every_tenth)}; For dispatch 1, kept every 10th reduction dim: {reduction_dim_every_tenth}')
+    print(f'\tlen {len(reduction_dim_every_tenth)}; For dispatch 1, pruned away reduction dims < 20: {greaterThan20}')
+    
+  return exhaustive
+
+def case_3_exhaustive_options(c='n'):
+  row_dim_no_padding = list(filter(lambda x: dividesIntoOutputVectorEltCount(x), rowDimOptions()))
+  # STRICTLY CASE 3
+  row_dim_w_o_case_1 = list(set(rowDimOptions()).difference(set(row_dim_no_padding)))
+  reduction_dim_no_padding = list(filter(lambda x: dividesIntoInputVectorEltCount(x), reductionDimOptions()))
+  exhaustive = list(product(row_dim_w_o_case_1, reduction_dim_no_padding))
+  if c=='p':
+    print(f'case_3_exhaustive_options')
+    print(f'\tlen {len(row_dim_w_o_case_1)}; row_dim_w_o_case_1: {row_dim_w_o_case_1}')
+    print(f'\tlen {len(reduction_dim_no_padding)}; reduction_dim_no_padding: {reduction_dim_no_padding}')
+  
+  return exhaustive
+
+def case_3_pruned_options(c='n'):
+  row_dim_no_padding = list(filter(lambda x: dividesIntoOutputVectorEltCount(x), rowDimOptions()))
+  row_dim_less_than_or_equal_to_112 = list(filter(lambda x: x >= 112, rowDimOptions()))
+  w_o_case_1 = list(set(row_dim_less_than_or_equal_to_112).difference(set(row_dim_no_padding)))
+  # w_o_case_1 = list(set(rowDimOptions()).difference(set(row_dim_no_padding)))
+  # w_o_case_1.sort()
+  # everyOther = []
+  # for i in range(0,len(w_o_case_1)):
+  #   if (i%2) == 0:
+  #     everyOther.append(w_o_case_1[i])
+  reduction_dim_no_padding = list(filter(lambda x: dividesIntoInputVectorEltCount(x), reductionDimOptions()))
+  if (inputVectorEltCount % 2) != 0:
+    print(f"WARNING: M = {inputVectorEltCount} is NOT divisible by 2!")
+  reduction_dim_halve_options_for_double_buffering = list(filter(lambda x: x <= (inputVectorEltCount//2)+1, reduction_dim_no_padding))
+  greaterThan20 = list(filter(lambda x: x > 20, reduction_dim_halve_options_for_double_buffering))
+  pruned = list(product(w_o_case_1, greaterThan20))
+  if c == 'p':
+    print('case_3_pruned_options')
+   # print(f'\tlen {len(row_dim_less_than_or_equal_to_112)}; pruned away row_dims > 112: {row_dim_less_than_or_equal_to_112}')
+    print(f'\tlen {len(w_o_case_1)}; pruned away row dims from case 1: {w_o_case_1}')
+   # print(f'\tlen {len(everyOther)}; For dispatch 8, pruned away every other row dim: {everyOther}')
+    print(f'\tlen {len(reduction_dim_halve_options_for_double_buffering)}; reduction dims pruned for double buffering: {reduction_dim_halve_options_for_double_buffering}')
+    print(f'\tlen {len(greaterThan20)}; For dispatch 1, 9, pruned away reduction dims <= 20: {greaterThan20}')
+  return pruned
+
+# annotate a (row_dim, reduction_dim) pair with
+# total L1 space used for tiles
+# weight matrix tile size
+# total spaced used in L1
+# space remaining, etc.
+def annotateOption(tup):
+  return (tup, spaceForTiles(tup[0],tup[1]), weightMatTileSize(tup[0],tup[1]), spaceRemaining(tup[0],tup[1]))
+
+# helper for converting to CSV
+def convertAnnotationToFlatTuple(elt):
+  return (f'{0}-{elt[0][0]}-{elt[0][1]}',elt[0][0],elt[0][1],elt[1],elt[2],elt[3])
+
+# helper for converting to CSV
+def annotationColumnNames():
+  columns=['JSON Name','Row Dim','Reduction Dim','Space Needed in L1','Weight Matrix Tile Size','Space Remaining']
+  return columns
+
+# export annotated options to CSV
+def exportOptionsToCSV(caseNo, options):
+  as_flat_tuples=list(map(convertAnnotationToFlatTuple, options))
+  #print(as_flat_tuples)
+  df = pd.DataFrame(tuple(as_flat_tuples), columns=annotationColumnNames())
+  df['Case']=caseNo
+  df.to_csv(f'myrtle_tile_size_gen_out/{dispatchName}_case{caseNo}_searchSpace.csv',index=False)
+  print(f'Saved CSV to ./myrtle_tile_size_gen_out/{dispatchName}_case{caseNo}_searchSpace.csv')
+
+def case1():
+  print("\nCase 1: No padding in either dimension. --------------------------------|",end='')
+  print("\n                                                                        |")
+  print("Exhaustive:")
+  all_options = case_1_exhaustive_options('p')
+  annotated_options=list(map(lambda tup: annotateOption(tup), all_options))
+  valid_options = list(filter(lambda tup: smallEnough(tup[0][0], tup[0][1]),annotated_options))
+  # print(f'{len(valid_options)} options: {valid_options}')
+  print("Pruned:")
+  pruned_options = case_1_pruned_options('p')
+  annotated_options=list(map(lambda tup: annotateOption(tup), pruned_options))
+  valid_options = list(filter(lambda tup: smallEnough(tup[0][0], tup[0][1]),annotated_options))
+  print(f'{len(valid_options)} valid options')
+  exportOptionsToCSV(1, valid_options)
+  print("                                                                        |")
+  print("________________________________________________________________________|")
+
+def case2():
+  print("\nCase 2: Padding in column dimension ONLY. ------------------------------|", end='')
+  print("\n                                                                        |")
+  print("Exhaustive:")
+  all_options = case_2_exhaustive_options('p')
+  annotated_options=list(map(lambda tup: annotateOption(tup), all_options))
+  valid_options = list(filter(lambda tup: smallEnough(tup[0][0], tup[0][1]),annotated_options))
+  print(f'{len(valid_options)} valid options')
+  print("Pruned:")
+  pruned_options = case_2_pruned_options('p')
+  annotated_options=list(map(lambda tup: annotateOption(tup), pruned_options))
+  valid_options = list(filter(lambda tup: smallEnough(tup[0][0], tup[0][1]),annotated_options))
+  print(f'{len(valid_options)} valid options')
+  exportOptionsToCSV(2, valid_options)
+  print("                                                                          |")
+  print("__________________________________________________________________________|")
+
+def case3():
+  print("\nCase 3: Padding in row dimension ONLY. -----------------------------------|",end='')
+  print("\n                                                                          |")
+  print("Exhaustive:")
+  all_options = case_3_exhaustive_options('p')
+  annotated_options=list(map(lambda tup: annotateOption(tup), all_options))
+  valid_options = list(filter(lambda tup: smallEnough(tup[0][0], tup[0][1]),annotated_options))
+  print(f'{len(valid_options)} valid options')
+  print("Pruned:")
+  pruned_options = case_3_pruned_options('p')
+  annotated_options=list(map(lambda tup: annotateOption(tup), pruned_options))
+  valid_options = list(filter(lambda tup: smallEnough(tup[0][0], tup[0][1]),annotated_options))
+  print(f'{len(valid_options)} valid options')
+  exportOptionsToCSV(3, valid_options)
+  print("                                                                          |")
+  print("__________________________________________________________________________|")
+# def case1():
+#   print("Case 1: No padding in either dimension.")
+#   print("Exhaustive:")
+#   all_options = case_1_exhaustive_options()
+#   annotated_options=list(map(lambda tup: annotateOption(tup), all_options))
+#   valid_options = list(filter(lambda tup: smallEnough(tup[0][0], tup[0][1]),annotated_options))
+#   print(f'{len(valid_options)} options: {valid_options}')
+#   print("Pruned:")
+#   pruned_options = case_1_pruned_options()
+#   annotated_options=list(map(lambda tup: annotateOption(tup), pruned_options))
+#   valid_options = list(filter(lambda tup: smallEnough(tup[0][0], tup[0][1]),annotated_options))
+#   print(f'{len(valid_options)} options: {valid_options}')
+#   exportOptionsToCSV(2, valid_options)
+
+
+def main():
+    
+    print("\n|----------- CREATING TILE SIZE/SHAPE SEARCH SPACE FOR KERNEL ----------|",end='')
+    print("\n|                                                                       |",end='')
+    print(f'\n\t\t{dispatchName}')
+    print("\n|_______________________________________________________________________|\n", end='')
+    case1()
+    case2()
+    case3()
+
+if __name__ == "__main__":
+    main()
