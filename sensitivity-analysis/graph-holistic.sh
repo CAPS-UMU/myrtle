18e738e29fca68bfaf2a3e1a88b57cb2373ef713
EmilySillars <emsillars@gmail.com>
2025-09-06 07:06:44 +0200

    graph padded and unpadded dispatch 0

--- sensitivity-analysis/graph-holistic.sh
+++ sensitivity-analysis/graph-holistic.sh
@@ -32,7 +32,9 @@ echo "{}" > "test_output-disp-0-$mode.json"
 echo "{}" > "test_output-disp-1-$mode.json"
 echo "{}" > "test_output-disp-7-$mode.json"
 echo "{}" > "test_output-disp-8-$mode.json"
-fp="$dir/1x400x161wm-n-k-padding-k-graphing-logistics.csv" # dispatch 0
+# fp="$dir/1x400x161wm-n-k-graphing-logistics.csv" # dispatch 0
+# python3 myrtle.py "main\$async_dispatch_0_matmul_transpose_b_1x400x161_f64" $mode "test_output-disp-0-$mode.json" $fp
+fp="$dir/1x400x161wm-n-k-padding-k-graphing-logistics.csv" # dispatch 0 (padded)
 python3 myrtle.py "main\$async_dispatch_0_matmul_transpose_b_1x400x161_f64" $mode "test_output-disp-0-$mode.json" $fp
 fp="$dir/1x1200x400wm-n-k-graphing-logistics.csv"          # dispatch 1
 python3 myrtle.py "main\$async_dispatch_1_matmul_transpose_b_1x1200x400_f64" $mode "test_output-disp-1-$mode.json" $fp
@@ -42,10 +44,15 @@ fp="$dir/1x600x600wm-n-k-graphing-logistics.csv"           # dispatch 8
 python3 myrtle.py "main\$async_dispatch_8_matmul_transpose_b_1x600x600_f64" $mode "test_output-disp-8-$mode.json" $fp
 
 # generate graph actual vs predicted for Dispatch Time
-fp="$dir/1x400x161wm-n-k-padding-k-graphing-logistics.csv" # dispatch 0
+fp="$dir/1x400x161wm-n-k-graphing-logistics.csv" # dispatch 0
 fp2="1x400x161wm-n-k_searchSpace_analyzed-myrtle-$mode-ranking.csv"
 python3 -m graphing.actualVsPredicted $fp $fp2 $outputDir $mode $topX
 python3 -m graphing.xVsYs $fp $fp2 $outputDir $mode $topX
+
+# fp="$dir/1x400x161wm-n-k-padding-k-graphing-logistics.csv" # dispatch 0 (padded)
+# fp2="1x400x161wm-n-k_searchSpace_analyzed-myrtle-$mode-ranking.csv"
+# python3 -m graphing.actualVsPredicted $fp $fp2 $outputDir $mode $topX
+# python3 -m graphing.xVsYs $fp $fp2 $outputDir $mode $topX
 
 fp="$dir/1x1200x400wm-n-k-graphing-logistics.csv"          # dispatch 1
 fp2="1x1200x400wm-n-k_searchSpace_analyzed-myrtle-$mode-ranking.csv" 
