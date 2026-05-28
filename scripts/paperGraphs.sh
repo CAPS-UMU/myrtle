2cda00d38f4b864aed13f90a8227311b3a337ce5
EmilySillars <emsillars@gmail.com>
2026-05-28 12:09:08 +0200

    add graping script

---
+++ scripts/paperGraphs.sh
@@ -0,0 +1,115 @@
+# python concatCSVs.py $REMAINDER $DIVISOR "out/128x128x128-div-rem.csv"
+
+# 128x768x768wm-n-k_ss_c_rem_div_ana_pr_sel_sflt-timeout-results.csv
+# 128x768x768wm-n-k_ss_c_rem_div_ana_pruned-results-unskipped.csv
+# 192x384x384wm-n-k_ss_c_rem_div_ana_pr_sel_sflt-results-unskipped-151.csv
+# 192x384x384wm-n-k_ss_c_rem_div_ana_pr_sel_sflt-results-unskipped-25.csv
+# 192x384x384wm-n-k_ss_c_rem_div_ana_pr_sel_sflt-timeout-results.csv
+# 192x384x384wm-n-k_ss_c_rem_div_ana_pruned-39-min-ssr-configs-results.csv
+# 20-points
+# 20-points-div-rem
+# 256x256x256wm-n-k_ss_c_rem_div_ana_pruned-results.csv
+# 30x128x125wm-n-k_ss_c_rem_div_ana_pruned-50-results.csv
+# 384x384x384-div-rem-partial-results.csv
+# 384x384x384wm-n-k_ss_c_rem_ana_pruned-results-more.csv
+# 512x512x512wm-n-k_ss_c_rem_div_ana_pruned-results.csv
+
+DATAROOT="/home/emily/myrtle/sensitivity-analysis"
+BOTHTIMEDDIR="$DATAROOT/remainder-vs-divisor/both/timed"
+BOTHANNDIR="$DATAROOT/remainder-vs-divisor/both/untimed"
+
+# miniLM
+PARTIALRESULTS="$DATAROOT/remainder-vs-divisor/both/timed/384x384x384-div-rem-partial-results.csv"
+MOREDATA="$DATAROOT/remainder-vs-divisor/both/timed/384x384x384wm-n-k_ss_c_rem_ana_pruned-results-more.csv"
+
+# BGE Small
+bgeTIMEOUT="$BOTHTIMEDDIR/192x384x384wm-n-k_ss_c_rem_div_ana_pr_sel_sflt-timeout-results.csv"
+bge25="$BOTHTIMEDDIR/192x384x384wm-n-k_ss_c_rem_div_ana_pr_sel_sflt-results-unskipped-25.csv"
+bge151="$BOTHTIMEDDIR/192x384x384wm-n-k_ss_c_rem_div_ana_pr_sel_sflt-results-unskipped-151.csv"
+
+# Roberta
+robertaTIMEOUT="$BOTHTIMEDDIR/128x768x768wm-n-k_ss_c_rem_div_ana_pr_sel_sflt-timeout-results.csv"
+robertaREST="$BOTHTIMEDDIR/128x768x768wm-n-k_ss_c_rem_div_ana_pruned-results-unskipped.csv"
+
+# echo "files we need to concat..."
+# wc -l $PARTIALRESULTS
+# wc -l $MOREDATA
+# python concatCSVs.py $PARTIALRESULTS $MOREDATA "out/384x384x384-miniLM-results.csv"
+# wc -l "out/384x384x384-miniLM-results.csv"
+
+# wc -l $bgeTIMEOUT
+# wc -l $bge25
+# python concatCSVs.py $bgeTIMEOUT $bge25 "out/192x384x384-bgeSmall-25-timeout-results.csv"
+# wc -l "out/192x384x384-bgeSmall-25-timeout-results.csv"
+# wc -l "out/192x384x384-bgeSmall-25-timeout-results.csv"
+# wc -l $bge151
+# python concatCSVs.py "out/192x384x384-bgeSmall-25-timeout-results.csv" $bge151 "out/192x384x384-bgeSmall-results.csv"
+# wc -l "out/192x384x384-bgeSmall-results.csv"
+
+# wc -l $robertaTIMEOUT
+# wc -l $robertaREST
+# python concatCSVs.py $robertaTIMEOUT $robertaREST "out/128x768x768-roberta-results.csv"
+# wc -l "out/128x768x768-roberta-results.csv"
+
+# #bge
+# ls $bgeTIMEOUT
+# ls $bge25
+# ls $bge151
+# # Roberta
+# ls $robertaTIMEOUT
+# ls $robertaREST
+
+# 128x128x128-div-rem-ann.csv
+# 128x768x768wm-n-k_ss_c_rem_div_ana_pruned.csv
+# 192x384x384wm-n-k_ss_c_rem_div_ana_pruned.csv
+# 20-points
+# 20-points-div-rem
+# 256x256x256wm-n-k_ss_c_rem_div_ana_pruned.csv
+# 384x384x384wm-n-k_ss_c_rem_div_ana_pruned.csv
+
+TIMED=""
+ANNED=""
+
+bertTinyTIMED="$DATAROOT/remainder-vs-divisor/rem/timed/128x128x128wm-n-k_ss_c_rem_ana_pruned-results_lessThan_2048.csv"
+TIMED+=" $bertTinyTIMED"
+bertTinyANN="$BOTHANNDIR/128x128x128-div-rem-ann.csv"
+ANNED+=" $bertTinyANN"
+echo "128x128x128 $bertTinyTIMED $bertTinyANN" > "dims-csv-name-line-by-line-paper.input"
+#bash graph-paper-data.sh dims-csv-name-line-by-line-paper.input both
+
+
+bertMiniTIMED="$BOTHTIMEDDIR/256x256x256wm-n-k_ss_c_rem_div_ana_pruned-results.csv"
+TIMED+=" $bertMiniTIMED"
+bertMiniANN="$BOTHANNDIR/256x256x256wm-n-k_ss_c_rem_div_ana_pruned.csv"
+ANNED+=" $bertMiniANN"
+echo "256x256x256 $bertMiniTIMED $bertMiniANN" >> "dims-csv-name-line-by-line-paper.input"
+
+miniLMTIMED="$BOTHTIMEDDIR/384x384x384-miniLM-results.csv"
+TIMED+=" $miniLMTIMED"
+miniLMANN="$BOTHANNDIR/384x384x384wm-n-k_ss_c_rem_div_ana_pruned.csv"
+ANNED+=" $miniLMANN"
+echo "384x384x384 $miniLMTIMED $miniLMANN" >> "dims-csv-name-line-by-line-paper.input"
+#python createTimeoutRows.py "$miniLMTIMED" 8311517
+
+bgeSmallTIMED="$BOTHTIMEDDIR/192x384x384-bgeSmall-results.csv"
+TIMED+=" $bgeSmallTIMED"
+bgeSmallANN="$BOTHANNDIR/192x384x384wm-n-k_ss_c_rem_div_ana_pruned.csv"
+ANNED+=" $bgeSmallANN"
+echo "192x384x384 $bgeSmallTIMED $bgeSmallANN" >> "dims-csv-name-line-by-line-paper.input"
+
+#python createTimeoutRows.py "$bgeSmallTIMED" 4259722
+
+robertaTIMED="$BOTHTIMEDDIR/128x768x768-roberta-results.csv"
+TIMED+=" $robertaTIMED"
+robertaANN="$BOTHANNDIR/128x768x768wm-n-k_ss_c_rem_div_ana_pruned.csv"
+ANNED+=" $robertaANN"
+echo "128x768x768 $robertaTIMED $robertaANN" >> "dims-csv-name-line-by-line-paper.input"
+
+#python createTimeoutRows.py "$robertaTIMED" 11391161
+
+# echo $TIMED
+# ls $TIMED
+# echo $ANNED
+
+
+bash graph-paper-data.sh dims-csv-name-line-by-line-paper.input both
