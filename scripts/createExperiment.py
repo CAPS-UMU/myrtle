ca49d1c1f7af8b1e6d7a3bc1ecf7c1396bc1f309
EmilySillars <emsillars@gmail.com>
2026-08-12 16:50:48 -0400

    replace large SS with pruned SS to fit within git recommended file sizes

--- scripts/createExperiment.py
+++ scripts/createExperiment.py
@@ -118,13 +118,12 @@ def main():
         outputFiles.append(fullSS)
         requestedFiles.append(requestedFile)
         spmLayout = "regular"
-        #subprocess.call(['python3', '../myrtle/myrtle.py', kernelName, "sflt", "placeholder.json", "prune", spmLayout])
-        subprocess.call(['python3', '../myrtle/myrtle.py', kernelName, "sflt", "placeholder.json", "NoPrune", spmLayout])
+        subprocess.call(['python3', '../myrtle/myrtle.py', kernelName, "sflt", "placeholder.json", "prune", spmLayout])
+        #subprocess.call(['python3', '../myrtle/myrtle.py', kernelName, "sflt", "placeholder.json", "NoPrune", spmLayout])
         
 
     for (a,b) in zip(kernelNames,outputFiles):
         print(f'Generated SS for {a}')
-        print(f'Myrtle Output file is {b}')
     
     subprocess.call(['cp']+ requestedFiles+[ "-t", f"./{outputFolder}"])
     
